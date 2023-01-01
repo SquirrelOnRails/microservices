b572af92fda7f71e16691c440aeffd7d432ee021
Батраков Михаил <bmv@pkzdrav.ru>
2023-01-02 01:12:49 +0500

    feat(back): added ProductAPI ApplicationDbContext #1

--- sources/MangoRestraunt/Mango.Services.ProductAPI/Program.cs
+++ sources/MangoRestraunt/Mango.Services.ProductAPI/Program.cs
@@ -1,11 +1,19 @@
+using Mango.Services.ProductAPI.DbContexts;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
+#region services
 // Add services to the container.
+builder.Services.AddDbContext<ApplicationDbContext>(options => 
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+#endregion
 
 var app = builder.Build();
 

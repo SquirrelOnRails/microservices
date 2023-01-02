99f2a447264df979f8ea8869120d35e27dc5a4a4
Батраков Михаил <bmv@pkzdrav.ru>
2023-01-02 20:13:08 +0500

    feat(back): added AutoMapper #1

---
+++ sources/MangoRestraunt/Mango.Services.ProductAPI/AutoMapper/MappingConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Mango.Services.ProductAPI.Dto;
+using Mango.Services.ProductAPI.Models;
+
+namespace Mango.Services.ProductAPI.AutoMapper
+{
+    public class MappingConfiguration
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            var mappingConfig = new MapperConfiguration(config => 
+            {
+                config.CreateMap<ProductDto, Product>().ReverseMap();
+            });
+
+            return mappingConfig;
+        }
+    }
+}

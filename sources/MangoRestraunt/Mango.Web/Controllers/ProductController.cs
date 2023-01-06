678927716f53c6ca25e072dde16a2e05f139f1d0
Батраков Михаил <bmv@pkzdrav.ru>
2023-01-06 17:05:16 +0500

    feat(all): implemented CRUD on product in the frontend project #1

--- sources/MangoRestraunt/Mango.Web/Controllers/ProductController.cs
+++ sources/MangoRestraunt/Mango.Web/Controllers/ProductController.cs
@@ -26,5 +26,62 @@ namespace Mango.Web.Controllers
 
             return View(list);
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(ProductDto model)
+        {
+            if (ModelState.IsValid)
+            {
+                var response = await productService.CreateProductAsync<ResponseDto>(model);
+                if (response != null && response.IsSuccess)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+
+            return View(model);
+        }
+
+        public async Task<IActionResult> Edit(int productId)
+        {
+            var response = await productService.GetProductByIdAsync<ResponseDto>(productId);
+            
+            if (response != null && response.IsSuccess)
+            {
+                var product = JsonConvert.DeserializeObject<ProductDto>(Convert.ToString(response.Result) ?? "");
+                return View(product);
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(ProductDto model)
+        {
+            if (ModelState.IsValid)
+            {
+                var response = await productService.UpdateProductAsync<ResponseDto>(model);
+                if (response != null && response.IsSuccess)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+
+            return View(model);
+        }
+
+        public async Task<IActionResult> Delete(int productId)
+        {
+            var response = await productService.DeleteProductAsync<ResponseDto>(productId);
+
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
--- sources/MangoRestraunt/Mango.Web/Models/ProductDto.cs
+++ sources/MangoRestraunt/Mango.Web/Models/ProductDto.cs
@@ -1,15 +1,21 @@
-﻿namespace Mango.Web.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mango.Web.Models
 {
     public class ProductDto
     {
         public int ProductId { get; set; }
 
+        [Required]
         public string? Name { get; set; }
 
+        [Required]
         public double Price { get; set; }
 
+        [Required]
         public string? Description { get; set; }
 
+        [Required]
         public string? CategoryName { get; set; }
 
         public string? ImageUrl { get; set; }

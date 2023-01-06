def0f44bf8fa8cb433a1bb18b44c253136741217
Батраков Михаил <bmv@pkzdrav.ru>
2023-01-06 11:54:10 +0500

    feat(front): added products view and action #1

---
+++ sources/MangoRestraunt/Mango.Web/Controllers/ProductController.cs
@@ -0,0 +1,30 @@
+﻿using Mango.Web.Models;
+using Mango.Web.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Mango.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductService productService { get; }
+
+        public ProductController(IProductService productService)
+        {
+            this.productService = productService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            List<ProductDto>? list = new();
+
+            var response = await productService.GetAllProductsAsync<ResponseDto>();
+            if (response != null && response.IsSuccess)
+            {
+                list = JsonConvert.DeserializeObject<List<ProductDto>>(Convert.ToString(response.Result) ?? "");
+            }
+
+            return View(list);
+        }
+    }
+}
